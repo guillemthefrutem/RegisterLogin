4eb80b16bf58041422a149d8ecec9a9c93226a05
Aster <jlfonpra@gmail.com>
2024-02-14 11:00:35 +0100

    aster4

--- RegisterLogin/Form2.cs
+++ RegisterLogin/Form2.cs
@@ -15,6 +15,7 @@ namespace RegisterLogin
         public Form2()
         {
             InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
         }
 
         private void Form2_Load(object sender, EventArgs e)
@@ -54,7 +55,29 @@ namespace RegisterLogin
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            if (AsterTextBoxUser.Text == "" || AsterTextBoxEmail.Text == "" || AsterTextBoxPswd1.Text == "" || AsterTextBoxPswd2.Text == "")
+            {
+                MessageBox.Show("Se necesitan rellenar todos los campos", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                if (AsterTextBoxPswd1.Text != AsterTextBoxPswd2.Text)
+                {
+                    MessageBox.Show("No coinciden las contraseñas", "Error", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Registro concedido con éxito", "Check", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    AsterTextBoxUser.Text = "";
+                    AsterTextBoxEmail.Text = "";
+                    AsterTextBoxPswd1.Text = "";
+                    AsterTextBoxPswd2.Text = "";
+                    this.Dispose();
+                    Form1 f1 = new Form1();
+                    f1.ShowDialog();
+                }
+            }
+            
         }
 
         private void AsterTextBox3_TextChanged(object sender, EventArgs e)
@@ -64,7 +87,13 @@ namespace RegisterLogin
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            AsterTextBoxUser.Text = "";
+            AsterTextBoxEmail.Text = "";
+            AsterTextBoxPswd1.Text = "";
+            AsterTextBoxPswd2.Text = "";
+            this.Dispose();
+            Form1 f1 = new Form1();
+            f1.ShowDialog();
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
